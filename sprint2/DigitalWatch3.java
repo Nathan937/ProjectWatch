598e6813129da764d9f05ca831c53b5486582aa6
pyrogenium <lukaaude@gmail.com>
2018-01-05 11:11:58 +0100

    sprint 2

---
+++ sprint2/DigitalWatch3.java
@@ -0,0 +1,111 @@
+
+import java.awt.*;
+
+import java.util.*;
+
+import javax.swing.*;
+
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+
+public class DigitalWatch3 {
+
+	public DigitalWatch3() {
+		JFrame frm = new JFrame();
+		
+		SimpleDigitalClock clock1 = new SimpleDigitalClock();
+		frm.setBackground(Color.YELLOW);
+		frm.add(clock1);
+		frm.pack();
+		frm.setVisible(true);
+	}
+
+	static class SimpleDigitalClock extends JPanel {
+		String stringTime;
+		int hour, minute, second, day, month, year;
+		char date;
+		String aHour = "";
+		String bMinute = "";
+		String cSecond = "";
+		String dDate = "";
+		String eDay = "";
+		String fMonth = "";
+		String gYear = "";
+
+		public void setStringTime(String abcdefg) {
+			this.stringTime = abcdefg;
+		}
+
+		public int Number(int a, int b) {
+			return (a <= b) ? a : b;
+		}
+
+		SimpleDigitalClock() {
+			setVisible(true);
+			Timer t = new Timer(1000, new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					repaint();
+				}
+			});
+			t.start();
+		}
+
+		@Override
+		public void paintComponent(Graphics v) {
+			super.paintComponent(v);
+			Calendar rite = Calendar.getInstance();
+			hour = rite.get(Calendar.HOUR_OF_DAY);
+			minute = rite.get(Calendar.MINUTE);
+			second = rite.get(Calendar.SECOND);
+			day = rite.get(Calendar.DATE);
+			month = rite.get(Calendar.MONTH) + 1;
+			year = rite.get(Calendar.YEAR);
+			date = (char) rite.get(Calendar.DATE);
+
+			if (hour < 10) {
+				this.aHour = "0";
+			}
+			if (hour >= 10) {
+				this.aHour = "";
+			}
+			if (minute < 10) {
+				this.bMinute = "0";
+			}
+			if (minute >= 10) {
+				this.bMinute = "";
+			}
+			if (second < 10) {
+				this.cSecond = "0";
+			}
+			if (second >= 10) {
+				this.cSecond = "";
+			}
+			if (day >= 10) {
+				this.eDay = "";
+			}
+			if (day < 10) {
+				this.eDay = "0";
+			}
+			if (month >= 10) {
+				this.fMonth = "";
+			}
+			if (month < 10) {
+				this.fMonth = "0";
+			}
+			setStringTime(aHour + hour + ":" + bMinute + minute + ":" + cSecond + second + "  " + eDay + day + "/"
+					+ fMonth + month + "/" + year + date);
+			v.setColor(Color.BLACK);
+			int length = Number(this.getWidth(), this.getHeight());
+			Font Font1 = new Font("SansSerif", Font.PLAIN, length / 5);
+			
+			v.setFont(Font1);
+			v.drawString(stringTime, (int) length / 6, length / 2);
+		}
+
+		@Override
+		public Dimension getPreferredSize() {
+			return new Dimension(420, 200);
+		}
+	}
+}
---
+++ sprint2/MainUhr.java
@@ -0,0 +1,117 @@
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+public class MainUhr {
+	static JFrame frame;
+	static JMenuBar menu;
+	static JMenu analogUhr;
+	static JMenuItem analoguhr;
+	static JMenu digitalUhr;
+	static JMenuItem digitaluhr;
+	static JMenuItem mehrereDigitaleuhren;
+	static JMenu mehrereDigitaleUhren;
+	static JMenuItem digitalUhrZeit;
+
+	public static void menu() {
+		menu = new JMenuBar();
+		analogUhr = new JMenu("Analog Uhr");
+		analoguhr = new JMenuItem("Analog Uhr");
+		digitalUhr = new JMenu("Digital Uhr");
+		digitaluhr = new JMenuItem("Digital Uhr");
+		mehrereDigitaleUhren = new JMenu("Mehrere Digitale Uhren");
+		mehrereDigitaleuhren = new JMenuItem("Mehrere Digitale Uhren");
+
+		frame.setJMenuBar(menu);
+		menu.add(analogUhr);
+		menu.add(digitalUhr);
+		menu.add(mehrereDigitaleUhren);
+		analogUhr.add(analoguhr);
+		mehrereDigitaleUhren.add(mehrereDigitaleuhren);
+		digitalUhr.add(digitaluhr);
+	}
+
+	public static void fenster() {
+		frame = new JFrame("Main Project Watch");
+		frame.setBounds(100, 100, 400, 300);
+		
+
+	}
+
+	// montre digitale 
+	
+	
+
+	public static void ListenerDigitalUhr() {
+
+		digitaluhr.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("New DigitalWatch is coming");
+				new DigitalWatch3();
+				
+			}
+		});
+
+	}
+
+	// montre analogue
+	public static void ListenerAnalogWatch() {
+
+		analoguhr.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("New AnalogWatch is coming");
+
+				// remplacer ici le nombre Analowatchx() ( x allant de 0 à 4 )
+				new Analogwatch();
+
+			}
+		});
+
+	}
+
+	/*
+	 * remplacer par notre multitude de montre trop classe // enlever le commentaire
+	 * tout en bas pour que cela fonctionne
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * public static void ListenerMehrereDigitaleUhren() {
+	 * 
+	 * mehrereDigitaleuhren.addActionListener(new ActionListener() { public void
+	 * actionPerformed(ActionEvent arg0) { DisplayMehrereDigitalUhr frame = new
+	 * DisplayMehrereDigitalUhr(); } });
+	 * 
+	 * }
+	 */
+
+	public static void main(String[] args) {
+
+		fenster();
+		menu();
+		ListenerDigitalUhr();
+		ListenerAnalogWatch();
+		// ListenerMehrereDigitaleUhren();
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+
+	}
+
+}
